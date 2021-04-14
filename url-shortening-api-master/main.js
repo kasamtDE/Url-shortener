4a7dd901913e0e648871a51c863d4e9600ad7f3c
Kaan <74673974+kasamtDE@users.noreply.github.com>
2021-04-14 17:17:40 +0300

    Add files via upload

---
+++ url-shortening-api-master/main.js
@@ -0,0 +1,100 @@
+let toggle=document.querySelector(".toggle")
+let navbar=document.querySelector(".navbar")
+let nav=document.querySelector("nav")
+let input=document.querySelector(".input")
+let shortenIt=document.querySelector(".shorten-it")
+let error=document.querySelector(".error")
+let errorBorder=document.querySelector(".error-border")
+let linkContainer=document.querySelector(".link-container")
+let shortenBtn=document.getElementById("shorten-btn")
+let copy=document.querySelector(".copy")
+
+/* toggle nav*/
+toggle.addEventListener("click",function(){
+    navbar.classList.toggle("active")
+    toggle.classList.toggle("clicked")
+})
+
+
+
+let handler = async () =>{
+  let url=input.value
+  let shorten=shortenBtn.textContent="Shortening..."
+  let preventEdit=input.disabled=true
+    await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`)
+    .then(response =>response.json()
+    .then(data => {
+        let editAgain=input.disabled=false
+        let shortenBack=shortenBtn.textContent="Shorten It!"
+        let short=data.result.short_link
+        console.log(data.result)
+        let newDiv=document.createElement("div") /*container for given url*/
+        newDiv.classList.add("shortened-container")
+        linkContainer.append(newDiv)
+        var newlink=document.createElement("li") /*shortened url*/
+        newlink.classList.add("shortened-link")
+        newlink.textContent=short
+        newDiv.append(newlink)
+        let newButton=document.createElement("button")
+        newButton.textContent="Copy"
+        newButton.classList.add("copy")
+        newDiv.append(newButton)
+        newButton.addEventListener("click",function(e){
+          e.target.textContent="Copied!"
+          let textArea  = document.createElement('textarea');
+          textArea.width  = "1px"; 
+          textArea.height = "1px";
+          textArea.background =  "transparents" ;
+          textArea.value = newlink.textContent;
+          document.body.append(textArea);
+          textArea.select();
+          textArea.setSelectionRange(0, 99999)
+          document.execCommand('copy');
+          document.body.removeChild(textArea);
+        })
+    })
+    );
+
+    if(input.value==="" || input.value===" "){
+      error.style.visibility="visible"
+      errorBorder.style.visibility="visible"
+    }
+    else{
+      function isValidURL(url) {
+        link = url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+       return (link !== null)
+      };
+      if(isValidURL(input.value)){
+        let newDiv=document.createElement("div")
+        linkContainer.append(newDiv)
+        let originaLink=input.value
+        let newLi=document.createElement("li")
+        newLi.classList.add("original-link")
+        newLi.innerText=originaLink
+        newDiv.append(newLi)
+        input.value=""
+        error.style.visibility=""
+        errorBorder.style.visibility=""
+  
+      }else{
+        error.style.visibility="visible"
+        errorBorder.style.visibility="visible"
+      }
+     }
+  };
+
+
+
+shortenIt.addEventListener("click",handler)
+input.addEventListener("keydown",(event)=>{
+    if(event.code==="Enter"){
+      handler()
+    };
+  });
+
+
+
+   
+
+
+
