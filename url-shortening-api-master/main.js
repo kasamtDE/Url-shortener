57552a04808880e7f020bf2174bb49b3380e6ffa
Kaan <74673974+kasamtDE@users.noreply.github.com>
2021-04-14 19:07:09 +0300

    Update main.js

--- url-shortening-api-master/main.js
+++ url-shortening-api-master/main.js
@@ -19,51 +19,55 @@ toggle.addEventListener("click",function(){
 
 let handler = async () =>{
   let url=input.value
-  let shorten=shortenBtn.textContent="Shortening..."
-  let preventEdit=input.disabled=true
-    await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`)
-    .then(response =>response.json()
-    .then(data => {
-        let editAgain=input.disabled=false
-        let shortenBack=shortenBtn.textContent="Shorten It!"
-        let short=data.result.short_link
-        console.log(data.result)
-        let newDiv=document.createElement("div") /*container for given url*/
-        newDiv.classList.add("shortened-container")
-        linkContainer.append(newDiv)
-        var newlink=document.createElement("li") /*shortened url*/
-        newlink.classList.add("shortened-link")
-        newlink.textContent=short
-        newDiv.append(newlink)
-        let newButton=document.createElement("button")
-        newButton.textContent="Copy"
-        newButton.classList.add("copy")
-        newDiv.append(newButton)
-        newButton.addEventListener("click",function(e){
-          e.target.textContent="Copied!"
-          let textArea  = document.createElement('textarea');
-          textArea.width  = "1px"; 
-          textArea.height = "1px";
-          textArea.background =  "transparents" ;
-          textArea.value = newlink.textContent;
-          document.body.append(textArea);
-          textArea.select();
-          textArea.setSelectionRange(0, 99999)
-          document.execCommand('copy');
-          document.body.removeChild(textArea);
-        })
-    })
-    );
+    
+  function isValidURL(url) {
+    link = url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+   return (link !== null)
+  };
+  
+  if(url==="" || url===" " || !isValidURL(url)){
+    error.style.visibility="visible"
+    errorBorder.style.visibility="visible"
+  }else{
+    error.style.visibility=""
+    errorBorder.style.visibility=""
+    let shorten=shortenBtn.textContent="Shortening..."
+    let preventEdit=input.disabled=true
+      await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`)
+      .then(response =>response.json()
+      .then(data => {
+          let editAgain=input.disabled=false
+          let shortenBack=shortenBtn.textContent="Shorten It!"
+          let short=data.result.short_link
+          console.log(data.result)
+          let newDiv=document.createElement("div") /*container for given url*/
+          newDiv.classList.add("shortened-container")
+          linkContainer.append(newDiv)
+          var newlink=document.createElement("li") /*shortened url*/
+          newlink.classList.add("shortened-link")
+          newlink.textContent=short
+          newDiv.append(newlink)
+          let newButton=document.createElement("button")
+          newButton.textContent="Copy"
+          newButton.classList.add("copy")
+          newDiv.append(newButton)
+          newButton.addEventListener("click",function(e){
+            e.target.textContent="Copied!"
+            let textArea  = document.createElement('textarea');
+            textArea.width  = "1px"; 
+            textArea.height = "1px";
+            textArea.background =  "transparents" ;
+            textArea.value = newlink.textContent;
+            document.body.append(textArea);
+            textArea.select();
+            textArea.setSelectionRange(0, 99999)
+            document.execCommand('copy');
+            document.body.removeChild(textArea);
+          })
+      })
+      );
 
-    if(input.value==="" || input.value===" "){
-      error.style.visibility="visible"
-      errorBorder.style.visibility="visible"
-    }
-    else{
-      function isValidURL(url) {
-        link = url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-       return (link !== null)
-      };
+  }
       if(isValidURL(input.value)){
         let newDiv=document.createElement("div")
         linkContainer.append(newDiv)
@@ -72,16 +76,10 @@ let handler = async () =>{
         newLi.classList.add("original-link")
         newLi.innerText=originaLink
         newDiv.append(newLi)
-        input.value=""
-        error.style.visibility=""
-        errorBorder.style.visibility=""
-  
-      }else{
-        error.style.visibility="visible"
-        errorBorder.style.visibility="visible"
+        input.value="" 
       }
      }
-  };
+  ;
 
 
 
@@ -91,6 +89,7 @@ input.addEventListener("keydown",(event)=>{
       handler()
     };
   });
+
 
 
 
