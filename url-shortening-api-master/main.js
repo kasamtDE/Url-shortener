f073b61d6a6c0721df4b01635ac074c37b479de2
Kaan <74673974+kasamtDE@users.noreply.github.com>
2021-04-15 22:00:16 +0300

    Update main.js

--- url-shortening-api-master/main.js
+++ url-shortening-api-master/main.js
@@ -18,11 +18,11 @@ toggle.addEventListener("click",function(){
 
 
 let handler = async () =>{
-  let url=input.value
+  let url=input.value;
     
   function isValidURL(url) {
     link = url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-   return (link !== null)
+   return (link !== null);
   };
   
   if(url==="" || url===" " || !isValidURL(url)){
@@ -33,60 +33,54 @@ let handler = async () =>{
     errorBorder.style.visibility=""
     let shorten=shortenBtn.textContent="Shortening..."
     let preventEdit=input.disabled=true
-      await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`)
-      .then(response =>response.json()
-      .then(data => {
-          let editAgain=input.disabled=false
-          let shortenBack=shortenBtn.textContent="Shorten It!"
-          let short=data.result.short_link
-          console.log(data.result)
-          let newDiv=document.createElement("div") /*container for given url*/
-          newDiv.classList.add("shortened-container")
-          linkContainer.append(newDiv)
-          var newlink=document.createElement("li") /*shortened url*/
-          newlink.classList.add("shortened-link")
-          newlink.textContent=short
-          newDiv.append(newlink)
-          let newButton=document.createElement("button")
-          newButton.textContent="Copy"
-          newButton.classList.add("copy")
-          newDiv.append(newButton)
-          newButton.addEventListener("click",function(e){
-            e.target.textContent="Copied!"
-            let textArea  = document.createElement('textarea');
-            textArea.width  = "1px"; 
-            textArea.height = "1px";
-            textArea.background =  "transparents" ;
-            textArea.value = newlink.textContent;
-            document.body.append(textArea);
-            textArea.select();
-            textArea.setSelectionRange(0, 99999)
-            document.execCommand('copy');
-            document.body.removeChild(textArea);
-          })
-      })
-      );
-
-  }
-      if(isValidURL(input.value)){
-        let newDiv=document.createElement("div")
-        linkContainer.append(newDiv)
-        let originaLink=input.value
-        let newLi=document.createElement("li")
-        newLi.classList.add("original-link")
-        newLi.innerText=originaLink
-        newDiv.append(newLi)
-        input.value="" 
-      }
-     }
-  ;
-
+     let fetching= await axios(`https://api.shrtco.de/v2/shorten?url=${url}`)
+     .then(data => {
+      let editAgain=input.disabled=false
+      let shortenBack=shortenBtn.textContent="Shorten It!"
+      let shortLink=data.data.result.short_link
+      let newDiv=document.createElement("div") /*container for given url*/
+      newDiv.classList.add("shortened-container")
+      linkContainer.append(newDiv)
+      var newlink=document.createElement("li") /*shortened url*/
+      newlink.classList.add("shortened-link")
+      newlink.textContent=shortLink
+      newDiv.append(newlink)
+      let newButton=document.createElement("button")
+      newButton.textContent="Copy"
+      newButton.classList.add("copy")
+      newDiv.append(newButton)
+      newButton.addEventListener("click",function(e){
+        e.target.textContent="Copied!"
+        let textArea  = document.createElement('textarea');
+        textArea.width  = "1px"; 
+        textArea.height = "1px";
+        textArea.background =  "transparents" ;
+        textArea.value = newlink.textContent;
+        document.body.append(textArea);
+        textArea.select();
+        textArea.setSelectionRange(0, 99999)
+        document.execCommand('copy');
+        document.body.removeChild(textArea);
+         });
+      }).catch(rejected=> console.log(rejected))
+    };
+  if(isValidURL(input.value)){
+    let newDiv=document.createElement("div")
+    linkContainer.append(newDiv)
+    let originaLink=input.value
+    let newLi=document.createElement("li")
+    newLi.classList.add("original-link")
+    newLi.innerText=originaLink
+    newDiv.append(newLi)
+    input.value="" 
+  };
+};
 
 
 shortenIt.addEventListener("click",handler)
 input.addEventListener("keydown",(event)=>{
     if(event.code==="Enter"){
-      handler()
+      handler();
     };
   });
 
